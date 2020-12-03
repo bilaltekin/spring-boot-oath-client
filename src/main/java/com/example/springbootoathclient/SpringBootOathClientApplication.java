648fe625c0b634b92b59e061b6fd8d12a84546a3
Bilal Tekin <bt.ce21@gmail.com>
2020-12-03 12:08:18 +0300

    first commit

---
+++ src/main/java/com/example/springbootoathclient/SpringBootOathClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootoathclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOathClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOathClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootoathclient/SpringBootOathClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootoathclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOathClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
